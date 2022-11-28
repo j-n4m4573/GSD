72cf7c75f6551a9cac0f2a772209b3670efb7abc
Maurice G <mauriceg@maurices-air.lan>
2022-11-28 09:58:04 -0800

    the journey begins...

---
+++ GSD/GSDApp.swift
@@ -0,0 +1,17 @@
+//
+//  GSDApp.swift
+//  GSD
+//
+//  Created by Jamar Gibbs on 11/26/22.
+//
+
+import SwiftUI
+
+@main
+struct GSDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
